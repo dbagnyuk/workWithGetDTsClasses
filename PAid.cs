88ca4b2d60d4175be987db042768144208a74921
dbagnyuk <dbagnyuk@gmail.com>
2019-10-23 10:09:59 +0200

    commit

---
+++ PAid.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using workWithGetDTsClasses.ISca;
+
+namespace workWithGetDTsClasses
+{
+    public class PAid
+    {
+        long personalAccount;
+        List<TDid> tdsList = new List<TDid>();
+        List<long> tdcWithLookedSrv = new List<long>();
+        public void processInputData(long inputPA, GetTDsOutput inputData)
+        {
+            this.personalAccount = inputPA;
+
+            foreach (var tdcs in inputData.TDs)
+            {
+                TDid oneTDid = new TDid();
+                oneTDid.processInputData(tdcs.TdId, tdcs.Services);
+
+                tdsList.Add(oneTDid);
+            }
+        }
+        public void writePAtoConsole()
+        {
+            Console.WriteLine($"TDIds with Services for PA {this.personalAccount}:\n");
+            //Console.WriteLine(this.personalAccount + "\n");
+            this.tdsList.ForEach(i => i.writeTDtoConsole());
+        }
+        public void writeTDwithSRVtoConsole(string lookedSrv)
+        {
+            if (!string.IsNullOrEmpty(lookedSrv))
+            {
+                Console.WriteLine($"\n\nThe TDs which have the valid service {lookedSrv}:");
+                tdcWithLookedSrv.ForEach(i => Console.Write("{0}\n", i));
+            }
+        }
+        public void lookupSRV(string lookedSrv)
+        {
+            if (!string.IsNullOrEmpty(lookedSrv))
+            {
+                foreach (TDid i in tdsList)
+                {
+                    if (i.lookupSRV(lookedSrv) != 0)
+                        tdcWithLookedSrv.Add(i.lookupSRV(lookedSrv));
+                }
+            }
+        }
+        public void sortTDsList(string sortTDsType, string sortSRVsType)
+        {
+            if (sortTDsType == "asc")
+            {
+                tdsList = tdsList.OrderBy(p => p.terminalDevice).ToList();
+                tdcWithLookedSrv = tdcWithLookedSrv.OrderBy(p => p).ToList();
+                this.tdsList.ForEach(i => i.sortSrvList(sortSRVsType));
+            }
+            else if (sortTDsType == "desc")
+            {
+                tdsList = tdsList.OrderByDescending(p => p.terminalDevice).ToList();
+                tdcWithLookedSrv = tdcWithLookedSrv.OrderByDescending(p => p).ToList();
+                this.tdsList.ForEach(i => i.sortSrvList(sortSRVsType));
+            }
+        }
+    }
+}
---
+++ TDid.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workWithGetDTsClasses
+{
+    public class TDid
+    {
+        public long terminalDevice;
+        bool haveLookedSrv = false;
+        List<SRV> srvList = new List<SRV>();
+        public void processInputData(long inputTD, string srvWhole)
+        {
+            this.terminalDevice = inputTD;
+
+            string[] srvSplited = srvWhole.Split('|');
+            foreach (var service in srvSplited)
+            {
+                SRV oneSRV = new SRV();
+                oneSRV.parseService(service);
+
+                srvList.Add(oneSRV);
+            }
+        }
+        public void writeTDtoConsole()
+        {
+            Console.WriteLine("\t" + this.terminalDevice);
+            this.srvList.ForEach(i => i.writeSRVtoConsole());
+        }
+        public long lookupSRV(string lookedSRV)
+        {
+            foreach(SRV i in srvList)
+            {
+                if (i.lookupSRV(lookedSRV) && i.serviceValidity())
+                    haveLookedSrv = true;
+            }
+
+            if (this.haveLookedSrv)
+                return this.terminalDevice;
+            else return 0;
+        }
+        public void sortSrvList(string sortSRVsType)
+        {
+            if (sortSRVsType == "asc")
+                srvList = srvList.OrderBy(p => p.srvName).ToList();
+            else if (sortSRVsType == "desc")
+                srvList = srvList.OrderByDescending(p => p.srvName).ToList();
+        }
+    }
+}
